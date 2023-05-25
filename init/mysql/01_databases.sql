afd4770977f1d3c8497422285aac8c46cdb928b4
mvandoren2 <54283641+mvandoren2@users.noreply.github.com>
2023-05-25 09:30:10 -0400

    Update 01_databases.sql

--- init/mysql/01_databases.sql
+++ init/mysql/01_databases.sql
@@ -4,5 +4,9 @@ CREATE DATABASE IF NOT EXISTS `npm_secondary_db`;
 CREATE DATABASE IF NOT EXISTS `guacamole_db`;
 
 -- create root user and grant rights
-CREATE USER 'se-utilities'@'localhost' IDENTIFIED BY '53u71l17135';
-GRANT ALL ON *.* TO 'se-utilities'@'localhost';
+CREATE USER 'se-utilities'@'proxy' IDENTIFIED BY '53u71l17135';
+CREATE USER 'se-utilities'@'rw-proxy' IDENTIFIED BY '53u71l17135';
+CREATE USER 'se-utilities'@'guacamole' IDENTIFIED BY '53u71l17135';
+GRANT ALL ON npm_primary_db.* TO 'se-utilities'@'proxy';
+GRANT ALL ON npm_secondary_db.* TO 'se-utilities'@'rw-proxy';
+GRANT ALL ON guacamole_db.* TO 'se-utilities'@'guacamole';
