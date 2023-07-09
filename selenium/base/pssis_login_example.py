bfab1c773924ba155cf9fe104043d4b92f900f69
mvandoren2 <54283641+mvandoren2@users.noreply.github.com>
2023-07-09 10:35:01 -0400

    Updated formatting testing out PyCharm's formatting option and optimizing the import statements at the top of the file.

--- selenium/base/pssis_login_example.py
+++ selenium/base/pssis_login_example.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-
 
 
 def main():
