e24605e7fa9b9fa8696ddb5c1536469c697abecc
mvandoren2 <54283641+mvandoren2@users.noreply.github.com>
2023-07-09 13:11:53 -0400

    Performed Initial Oauth 1.0 Test

--- api_scripts/sgy_advancement/access_test.py
+++ api_scripts/sgy_advancement/access_test.py
@@ -2,12 +2,12 @@ from requests_oauthlib import OAuth1Session
 
 
 def main():
-    test = OAuth1Session(
+    oauth_session = OAuth1Session(
         'aaa649f9fabadf81def11f492db08420064aad79a',
         client_secret='6b4a5b3bc85b7d0fe719e8465ee87661'
     )
     url = 'https://api.schoology.com/v1/sections/2316722634/assignments/5811649137'
-    r = test.get(url)
+    r = oauth_session.get(url)
     print(r.content)
 
 
