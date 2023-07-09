5e7eb7b9a886d8a659b7cce3ddae5fe0ddfb2670
mvandoren2 <54283641+mvandoren2@users.noreply.github.com>
2023-07-09 09:47:26 -0400

    Add gitIgnore and selenium example

---
+++ selenium/base/pssis_login_example.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+def main():
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+
+    driver.get("https://sg1.powerschoolsales.com/admin/pw.html")
+    username_field = driver.find_element(By.XPATH, '//*[@id="fieldUsername"]')
+    password_field = driver.find_element(By.XPATH, '//*[@id="fieldPassword"]')
+    sign_in_button = driver.find_element(By.XPATH, '//*[@id="btnEnter"]')
+
+    username_field.send_keys('mvandoren-admin')
+    password_field.send_keys('pwsc2023')
+    sign_in_button.click()
+
+
+if __name__ == '__main__':
+    main()
