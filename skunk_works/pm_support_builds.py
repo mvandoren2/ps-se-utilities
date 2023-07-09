69d400aaa068ddd6873c6d90cb13ed8aa363c25f
mvandoren2 <54283641+mvandoren2@users.noreply.github.com>
2023-07-09 10:52:39 -0400

    Reformatted pm support build file to be more pythonic.

--- skunk_works/pm_support_builds.py
+++ skunk_works/pm_support_builds.py
@@ -1,10 +1,10 @@
-from sqlite3 import ProgrammingError
-from progress.spinner import PieSpinner
-import time
-import mysql.connector
 import getpass
+import time
 
-# Actions 
+import mysql.connector
+
+
+# Actions
 def main():
     host = "opsdb-01.p2.pmstudent.powerschool.host"
     username = input("Username: ")
@@ -12,13 +12,9 @@ def main():
 
     try:
         conn = mysql.connector.connect(
-
-        host=host,
-
-        user=username,
-
-        password=password
-
+            host=host,
+            user=username,
+            password=password
         )
     except mysql.connector.errors.ProgrammingError:
         print("Invalid Password, Try Again")
@@ -29,20 +25,20 @@ def main():
 
     support_site_se_tenants = [
         {
-            "db_name" : "tx_applegrove",
-            "client_id" : "31009987"
+            "db_name": "tx_applegrove",
+            "client_id": "31009987"
         },
         {
-            "db_name" : "us_apple",
-            "client_id" : "3100683"
+            "db_name": "us_apple",
+            "client_id": "3100683"
         },
         {
-            "db_name" : "us_apple2",
-            "client_id" : "3100860"
+            "db_name": "us_apple2",
+            "client_id": "3100860"
         },
         {
-            "db_name" : "us_pssb",
-            "client_id" : "31009660"
+            "db_name": "us_pssb",
+            "client_id": "31009660"
         },
     ]
 
@@ -70,24 +66,27 @@ def main():
         WHERE
             client_id = {se_tenant['client_id']} 
             AND proc_name = 'etl_imp' LIMIT 1 ;"""
-        )
+                       )
 
         status_query_condition += f"'{se_tenant['db_name']}'"
-    
-    cursor.execute(f"select count(*) as pending from pmi_build.job_queue where status <> 'Done' and db_name not like '%pend' and build_group < 10 and db_name in ({status_query_condition});")
+
+    cursor.execute(
+        f"select count(*) as pending from pmi_build.job_queue where status <> 'Done' and db_name not like '%pend' and build_group < 10 and db_name in ({status_query_condition});")
     result_set = cursor.fetchone()
     pending_builds = result_set[0]
     while True:
         if pending_builds == 0:
             break
-        
+
         print(f"Pending Builds: {pending_builds}", end="\r")
         time.sleep(3)
-        cursor.execute(f"select count(*) as pending from pmi_build.job_queue where status not in ('Done', 'Error') and db_name not like '%pend' and build_group < 10 and db_name in ({status_query_condition});")
+        cursor.execute(
+            f"select count(*) as pending from pmi_build.job_queue where status not in ('Done', 'Error') and db_name not like '%pend' and build_group < 10 and db_name in ({status_query_condition});")
         result_set = cursor.fetchone()
         pending_builds = result_set[0]
 
     print(f"No further pending builds for {status_query_condition} tenants. Done!")
+
 
 if __name__ == '__main__':
     main()
